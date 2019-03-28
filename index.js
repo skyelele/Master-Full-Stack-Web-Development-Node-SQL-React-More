0fe1b1e56d2907a513e405c46d45ec31639b80ff
Skye Leahy <skyeleahy@protonmail.com>
2019-03-27 23:13:49 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const Dragon = require("./dragon");
+
+// Creating an instance called "fooey"
+// of the "Dragon" class.
+const fooey = new Dragon(new Date(), "fooey");
+const baloo = new Dragon(new Date(), "baloo");
+
+console.log(fooey);
+console.log(baloo);
