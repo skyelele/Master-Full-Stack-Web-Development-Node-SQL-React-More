c178af01720c834f583070bcebbbdead01d3cf67
Skye Leahy <skyeleahy@protonmail.com>
2019-03-28 12:15:42 -0700

    add modules + setTimeout() function practice

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const Dragon = require("./dragon");
+
+// Creating an instance called "fooey"
+// of the "Dragon" class.
+const fooey = new Dragon({
+  birthdate: new Date(),
+  nickname: "fooey"
+});
+const baloo = new Dragon({
+  birthdate: new Date(),
+  nickname: "baloo"
+});
+
+const mimar = new Dragon();
+
+setTimeout(() => {
+  const gooby = new Dragon();
+  console.log(gooby);
+}, 3000);
+
+console.log(fooey);
+console.log(baloo);
+console.log(mimar);
--- index.js
+++
@@ -1,9 +0,0 @@
-const Dragon = require("./dragon");
-
-// Creating an instance called "fooey"
-// of the "Dragon" class.
-const fooey = new Dragon(new Date(), "fooey");
-const baloo = new Dragon(new Date(), "baloo");
-
-console.log(fooey);
-console.log(baloo);
