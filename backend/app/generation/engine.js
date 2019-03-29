20841427f4dc103f3a007d1327f265d2c7328fa3
Skye Leahy <skyeleahy@protonmail.com>
2019-03-29 15:08:44 -0700

    add Node-Postgres Pool Verification and Generation Table/Storing Generations

--- backend/app/generation/engine.js
+++ backend/app/generation/engine.js
@@ -16,16 +16,22 @@ class GenerationEngine {
   }
 
   buildNewGeneration() {
-    this.generation = new Generation();
+    const generation = new Generation();
 
-    GenerationTable.storeGeneration(this.generation);
+    GenerationTable.storeGeneration(generation)
+      .then(({ generationId }) => {
+        this.generation = generation;
 
-    console.log("new generation", this.generation);
+        this.generation.generationId = generationId;
 
-    this.timer = setTimeout(
-      () => this.buildNewGeneration(),
-      this.generation.expiration.getTime() - Date.now()
-    );
+        console.log("new generation", this.generation);
+
+        this.timer = setTimeout(
+          () => this.buildNewGeneration(),
+          this.generation.expiration.getTime() - Date.now()
+        );
+      })
+      .catch(error => console.log(error));
   }
 }
 
--- backend/app/generation/table.js
+++ backend/app/generation/table.js
@@ -9,17 +9,34 @@ const pool = require("../../databasePool");
 class GenerationTable {
   // Static functions allow you to use them without
   // making an instance, which is not what we want
+
+  // Can force JS to interact with this function
+  // in a timed way that we have control over.
+  // Like an actual promise, we can fulfill
+  // and resolve the promise and return data
+  // or like an actual promise we can break
+  // the promise and reject it in the vent of an error.
+
+  // We can control the return value of the promise
+  // in the event of an error.
+
   static storeGeneration(generation) {
-    pool.query(
-      // $1 refers to 1st argument
-      // I.e. Insert 1st value of VALUES into
-      // generation(expiration)
-      "INSERT INTO generation(expiration) VALUES($1)",
-      [generation.expiration],
-      (error, response) => {
-        if (error) return console.error(error);
-      }
-    );
+    return new Promise((resolve, reject) => {
+      pool.query(
+        // $1 refers to 1st argument
+        // I.e. Insert 1st value of VALUES into
+        // generation(expiration)
+        "INSERT INTO generation(expiration) VALUES($1) RETURNING id",
+        [generation.expiration],
+        (error, response) => {
+          if (error) return reject(error);
+
+          const generationId = response.rows[0].id;
+
+          resolve({ generationId });
+        }
+      );
+    });
   }
 }
 
