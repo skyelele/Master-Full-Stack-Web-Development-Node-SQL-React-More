e8332018a78dd875fb43204e82a2edf0e556af8a
Skye Leahy <skyeleahy@protonmail.com>
2019-04-04 11:33:53 -0700

    implement fetch account dragons, account dragons reducer

--- frontend/src/actions/account.js
+++ frontend/src/actions/account.js
@@ -1,21 +1,27 @@
 import { ACCOUNT } from "./types";
 import { BACKEND } from "../config";
 
-const fetchFromAccount = ({ endpoint, options, SUCCESS_TYPE }) => dispatch => {
-  dispatch({ type: ACCOUNT.FETCH });
+export const fetchFromAccount = ({
+  endpoint,
+  options,
+  FETCH_TYPE,
+  ERROR_TYPE,
+  SUCCESS_TYPE
+}) => dispatch => {
+  dispatch({ type: FETCH_TYPE });
 
   return fetch(`${BACKEND.ADDRESS}/account/${endpoint}`, options)
     .then(response => response.json())
     .then(json => {
       if (json.type === "error") {
-        dispatch({ type: ACCOUNT.FETCH_ERROR, message: json.message });
+        dispatch({ type: ERROR_TYPE, message: json.message });
       } else {
         dispatch({ type: SUCCESS_TYPE, ...json });
       }
     })
     .catch(error => {
       dispatch({
-        type: ACCOUNT.FETCH_ERROR,
+        type: ERROR_TYPE,
         message: error.message
       });
     });
@@ -23,13 +29,15 @@ const fetchFromAccount = ({ endpoint, options, SUCCESS_TYPE }) => dispatch => {
 
 export const signup = ({ username, password }) =>
   fetchFromAccount({
-    endpoint: "logout",
+    endpoint: "signup",
     options: {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
       credentials: "include"
     },
+    FETCH_TYPE: ACCOUNT.FETCH,
+    ERROR_TYPE: ACCOUNT.FETCH_ERROR,
     SUCCESS_TYPE: ACCOUNT.FETCH_SUCCESS
   });
 
@@ -42,13 +50,17 @@ export const login = ({ username, password }) =>
       headers: { "Content-Type": "application/json" },
       credentials: "include"
     },
+    FETCH_TYPE: ACCOUNT.FETCH,
+    ERROR_TYPE: ACCOUNT.FETCH_ERROR,
     SUCCESS_TYPE: ACCOUNT.FETCH_SUCCESS
   });
 
 export const logout = () =>
   fetchFromAccount({
-    endpoint: "signup",
+    endpoint: "logout",
     options: { credentials: "include" },
+    FETCH_TYPE: ACCOUNT.FETCH,
+    ERROR_TYPE: ACCOUNT.FETCH_ERROR,
     SUCCESS_TYPE: ACCOUNT.FETCH_SUCCESS
   });
 
@@ -56,5 +68,7 @@ export const fetchAuthenticated = () =>
   fetchFromAccount({
     endpoint: "authenticated",
     options: { credentials: "include" },
+    FETCH_TYPE: ACCOUNT.FETCH,
+    ERROR_TYPE: ACCOUNT.FETCH_ERROR,
     SUCCESS_TYPE: ACCOUNT.FETCH_AUTHENTICATED_SUCCESS
   });
