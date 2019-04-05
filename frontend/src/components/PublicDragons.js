30f668e60c2666a00c7c80c1f72d413b16da91f7
Skye Leahy <skyeleahy@protonmail.com>
2019-04-05 15:30:53 -0700

    final commit!

--- frontend/src/components/PublicDragons.js
+++ frontend/src/components/PublicDragons.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPublicDragons } from "../actions/publicDragons";
+import { fetchAccountDragons } from "../actions/accountDragons";
 import { Link } from "react-router-dom";
 import PublicDragonRow from "./PublicDragonRow";
 
 class PublicDragons extends Component {
   componentDidMount() {
     this.props.fetchPublicDragons();
+    this.props.fetchAccountDragons();
   }
 
   render() {
@@ -29,5 +31,5 @@ class PublicDragons extends Component {
 
 export default connect(
   ({ publicDragons }) => ({ publicDragons }),
-  { fetchPublicDragons }
+  { fetchPublicDragons, fetchAccountDragons }
 )(PublicDragons);
